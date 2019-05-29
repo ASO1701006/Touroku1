0375d872d74c95c3636501e16976dec80d8cb865
ASO1701006 <1701006@st.asoujuku.ac.jp>
2019-05-29 15:36:32 +0900

    Initial commit

---
+++ app/src/main/java/jp/ac/asojuku/st/touroku1/request.kt
@@ -0,0 +1,12 @@
+package jp.ac.asojuku.st.touroku1
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class request : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_request)
+    }
+}
